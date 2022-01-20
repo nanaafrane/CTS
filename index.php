a65a4d21cfad72c952701a723417298389a2478e
allenaryee <allen.nii@icloud.com>
2022-01-20 12:10:03 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CTS </title>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+     <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css"> 
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="animate.css">
+    <link rel="stylesheet" href="style.css">
+    
+
+ 
+</head> 
+<style>
+    
+.parallax {
+    background-image: url("CTS BUS.jpg");
+    height: 100%;
+
+    min-height: 700px; 
+
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    }
+    
+</style>
+<body data-spy="scroll" data-target=".navbar" data-offset="50" onload="myFunction()"> 
+   
+<div class="parallax foo">
+ 
+<div class="hero-text" style="font-size:50px; text-align: center; position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);color: white;">
+           
+           <h1 class="animated rubberBand" >COPERATE TRANSPORT SERVICE</h1>
+            <a class="btn btn-success" style="text-align: center" href="login.php">Join Bus</a> 
+          </div> 
+ 
+</body>
+</html>
